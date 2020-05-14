9abbf60516f9b8867754e45a486e7322ae6c7df6
Beatriz Monteiro <monteiro.beatriz70@gmail.com>
2020-05-14 20:25:00 -0300

    ...

--- Fourirer Transformation/DFT.py
+++ Fourirer Transformation/DFT.py
@@ -1,26 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 14 17:37:47 2020
-
-@author: Fuso
-"""
-
-import numpy as np
-import imageio, time
-
-def DFT2D(f):
-    F = np.zeros(f.shape, dtype=np.complex64)
-    n,m = f.shape
-    
-    x = np.arange(n)
-    y = np.arange(m)
-    
-    for u in np.arange(n):
-        for v in np.arange(m):
-            
-            for x in np.arange(n):
-                fo# -*- coding: utf-8 -*-
-"""
 Created on Thu May 14 18:04:03 2020
 
 @author: Fuso
@@ -117,84 +96,6 @@ fourier_img = INV_DFT2D(fourier_img)
 fourier_img = fourier_img.astype(np.float32)
 
 #imageio.imwrite('output_img.png', fourier_img.astype(np.uint8))
-            
-print("Threshold=" , round(p2*T, 4))
-print("Filtered Coefficients", cnt)
-print("Original Mean=" , round(np.mean(img), 2))
-print("New Mean=", round(np.mean(fourier_img), 2))
-
-end = time.time()
-elapsed = end - start
-    
-print("Running time: "+ str(elapsed) + "sec")
-*n)
-
-def INV_DFT2D(f):
-    
-    F = np.zeros(f.shape, dtype=np.complex64)
-    n,m = f.shape
-    
-    start = time.time()
-    
-    x = np.arange(n)
-    y = np.arange(m)
-    
-    for u in np.arange(n):
-        for v in np.arange(m):
-            
-            for x in np.arange(n):
-                F[u,v] = np.sum(f[x,y]*np.exp((1j*2*np.pi)*(((u*x/n)+((v*y)/m)))))
-    
-    return F/np.sqrt(m*n)
-
-def find_2ndMax(f):
-    
-    p1 = np.max(f)
-    print(p1)
-    n,m = f.shape
-    
-    p2 = 0
-    
-    for i in range(n):
-        for j in range(m):
-            if f[i][j] > p2 and f[i][j] < p1:
-                p2 = f[i][j]
-    
-    return p2
-                
-            
-#MAIN
-
-#read image
-filename = str(input()).rstrip()
-input_img = imageio.imread(filename)
-
-T = float(input()) #coeficient
-
-img = np.array(input_img)
-img = img.astype(np.float32) #casting para realizar as funcoes
-
-start = time.time()
-
-#creates matrix for final image
-n,m = img.shape
-fourier_img = np.zeros((n,m), dtype=np.float)
-
-fourier_img = DFT2D(img)
-p2 = find_2ndMax(fourier_img)
-
-print(p2)
-
-cnt = 0
-
-for i in range(n):
-    for j in range(m):
-        if fourier_img[i][j] < p2*T:
-            fourier_img[i][j] = 0
-            cnt += 1
-
-fourier_img = INV_DFT2D(fourier_img)
-fourier_img = fourier_img.astype(np.float32)
             
 print("Threshold=" , round(p2*T, 4))
 print("Filtered Coefficients", cnt)
