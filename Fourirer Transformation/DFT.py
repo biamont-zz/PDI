9235b0919fcd8009fe76f0af026526aaadfd8ad2
Beatriz Monteiro <monteiro.beatriz70@gmail.com>
2020-05-14 22:57:49 -0300

    coments the code

--- Fourirer Transformation/DFT.py
+++ Fourirer Transformation/DFT.py
@@ -17,42 +17,49 @@ github link: https://github.com/biamont/PDI/tree/master/Fourirer%20Transformatio
 import numpy as np
 import imageio, time, warnings
 
+#Fourier function
 def DFT2D(f):
     F = np.zeros(f.shape, dtype=np.complex64)
     n,m = f.shape
     
+    #creates row array for with values in range 0 to n for x and collum array with values 0 to m for y
     x = np.arange(n).reshape(n,1)
     y = np.arange(m).reshape(1, m)
     
+    #optmizes a 4 for loop to calculate the fourier function
     for u in np.arange(n):
         for v in np.arange(m):
            F[u,v] = np.sum(f[x,y]*np.exp((-1j*2*np.pi)*(((u*x/n)+((v*y)/m)))))
     
+    #normalizes result
     return F/np.sqrt(n*m)
 
+#Inverse Fourier function
 def INV_DFT2D(f):
     
     F = np.zeros(f.shape, dtype=np.complex64)
     n,m = f.shape
     
+    #creates row array for with values in range 0 to n for x and collum array with values 0 to m for y
     x = np.arange(n).reshape(n,1)
     y = np.arange(m).reshape(1, m)
     
+    #optmizes a 4 for loop to calculate the inverse fourier function
     for u in np.arange(n):
-        for v in np.arange(m):
-            
+        for v in np.arange(m):        
             F[u,v] = np.sum(f[x,y]*np.exp((1j*2*np.pi)*(((u*x/n)+((v*y)/m)))))
     
+    #normalizes result
     return F/np.sqrt(m*n)
 
 def find_2ndMax(f):
     
     p1 = np.max(f)
-#    print(p1)
     n,m = f.shape
     
     p2 = 0
     
+    #run throught matrix checking for the second highest absolut value
     for i in range(n):
         for j in range(m):
             if np.real(np.abs(f[i][j])) > p2 and np.real(np.abs(f[i][j])) != p1:
@@ -63,7 +70,7 @@ def find_2ndMax(f):
             
 #MAIN
 
-#read image
+#reads image
 filename = str(input()).rstrip()
 input_img = imageio.imread(filename)
 
@@ -74,26 +81,29 @@ img = img.astype(np.float32) #casting para realizar as funcoes
 
 start = time.time()
 
-#creates matrix for final image
+#creates matrix for image with fourier filter
 n,m = img.shape
 fourier_img = np.zeros((n,m), dtype=np.float)
 
+#gets image with fourier filter
 fourier_img = DFT2D(img)
 
+#gets second peak of the fourier image
 p2 = find_2ndMax(fourier_img)
-#print(p2)
-cnt = 0
 
+cnt = 0 #counter
+
+#turns to 0 all the values in fourier image below p2*T
 for i in range(n):
     for j in range(m):
         if abs(fourier_img[i][j]) < (p2*T):
             fourier_img[i][j] = 0
             cnt += 1
 
+#gets absolute valoue from image result of inverse fourier transformation
 fourier_img = abs(INV_DFT2D(fourier_img))
 
-#imageio.imwrite('output_img.png', fourier_img.astype(np.uint8))
-
+#ignore warnings -> to make it possible to post in run.codes
 warnings.filterwarnings("ignore")
          
 print("Threshold=%.4f" % (p2*T))
@@ -101,6 +111,7 @@ print("Filtered Coefficients=%d" % (cnt))
 print("Original Mean=%.2f" % (np.mean(img)))
 print("New Mean=%.2f" % (np.mean(fourier_img)))
 
+#calculates running time of the program
 end = time.time()
 elapsed = end - start
     
